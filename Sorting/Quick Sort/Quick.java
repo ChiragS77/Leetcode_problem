005622c8184580375d529d285268c4b654ae6bca
Chirag <deshmukhchirag178@gmail.com>
2026-05-29 14:05:59 +0530

    quickcommit

---
+++ Sorting/Quick Sort/Quick.java
@@ -0,0 +1,48 @@
+public class Quick{
+
+    public static void quickSort(int arr[], int si , int ei){
+        if(si>=ei){
+            return;
+        }
+
+        int pIdx = partition(arr,si , ei);
+        quickSort(arr, si, pIdx-1); //for left sort
+        quickSort(arr, pIdx, ei);  //for right sort
+
+
+    }
+
+    public static int partition(int arr[],int si , int ei){
+        int pivot = arr[ei];
+        int i = si -1;
+        for(int j = si; j <=ei ; j++){
+
+            if(arr[j]<pivot){
+                i++;
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        i++;
+        int temp = pivot;
+        arr[ei] = arr[i];
+        arr[i] = temp;
+        return i;
+    }
+
+    public static void printArr(int arr[]){
+
+        for(int i =0; i<arr.length; i++){
+            System.out.print(" "+arr[i]);
+        }
+    }
+    
+
+
+    public static void main(String[] args) {
+        int arr[] = {6,3,9,8,2,5};
+        quickSort(arr, 0, arr.length-1);
+        printArr(arr);
+    }
+}
